3622712faae8fe774815543a7417c0bfed189f52
DevAina <ainatolotriniavo7@gmail.com>
2024-11-13 14:55:38 +0300

    add Cpp_module_01/ex02

---
+++ CPP Module 01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trarijam <trarijam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/13 14:09:29 by trarijam          #+#    #+#             */
+/*   Updated: 2024/11/13 14:52:07 by trarijam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string	&stringREF = string;
+
+	std::cout << "Memory address of string: \033[1;33m" << &string << "\033[0m" << std::endl;
+	std::cout << "Memory address held by stringPTR: \033[1;32m" << stringPTR << "\033[0m" << std::endl;
+	std::cout << "Memory adrress held by stringREF: \033[1;31m" << &string << "\033[0m" << std::endl;
+	std::cout << "Value of the string: \033[34m" << string << "\033[0m" << std::endl;
+	std::cout << "Value pointed to by stringPTR: \033[34m" << *stringPTR << "\033[0m" << std::endl;
+	std::cout << "Value pointed by stringREF: \033[34m" << stringREF << "\033[0m" << std::endl;
+	return (0);
+}

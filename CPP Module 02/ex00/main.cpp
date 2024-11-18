1f818ecb887e14bde204ab1950155ed18ddaaf19
Tolotriniavo Aina Faniry Rarijamiadana <trarijam@c1r9s7.42antananarivo.mg>
2024-11-18 17:06:43 +0300

    Header CPP_MODULE_02/ex00/

--- CPP Module 02/ex00/main.cpp
+++ CPP Module 02/ex00/main.cpp
@@ -6,13 +6,14 @@
 /*   By: trarijam <trarijam@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 16:49:22 by trarijam          #+#    #+#             */
-/*   Updated: 2024/11/18 14:02:53 by trarijam         ###   ########.fr       */
+/*   Updated: 2024/11/18 17:06:05 by trarijam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main( void ) {
+int main( void )
+{
 	Fixed a;
 	Fixed b( a );
 	Fixed c;

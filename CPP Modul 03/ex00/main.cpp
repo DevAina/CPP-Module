ef40d5577f9ce19caa6a8f36cb56b989f6445a87
DevAina <ainatolotriniavo7@gmail.com>
2025-02-04 19:01:28 +0300

    Add: Module_03/ex00 (finish)

--- CPP Modul 03/ex00/main.cpp
+++ CPP Modul 03/ex00/main.cpp
@@ -3,20 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rtfa <trarijam@student.42antananarivo.mg>  +#+  +:+       +#+        */
+/*   By: trarijam <trarijam@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/04 17:38:44 by rtfa              #+#    #+#             */
-/*   Updated: 2025/02/04 17:43:01 by rtfa             ###   ########.fr       */
+/*   Created: 2025/02/04 18:53:31 by trarijam          #+#    #+#             */
+/*   Updated: 2025/02/04 18:56:14 by trarijam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-int	main()
+int main(void)
 {
-	std::string target("SCOOTER");
-	ClapTrap Bob;
+    ClapTrap defaultTrap;
+    defaultTrap.attack("Target1");
+	defaultTrap.takeDamage(5);
+    defaultTrap.beRepaired(3);
+    std::cout << std::endl;
 
-	Bob.attack(target);
-	return (0);
+    ClapTrap paramTrap("Bob");
+    paramTrap.attack("Target2");
+    paramTrap.takeDamage(8);
+    paramTrap.beRepaired(4);
+    std::cout << std::endl;
+
+    ClapTrap copyTrap(paramTrap);
+    copyTrap.attack("Target3");
+    copyTrap.takeDamage(2);
+    copyTrap.beRepaired(1);
+    std::cout << std::endl;
+
+    ClapTrap assignedTrap;
+    assignedTrap = paramTrap;
+    assignedTrap.attack("Target4");
+    assignedTrap.takeDamage(3);
+    assignedTrap.beRepaired(2);
+    std::cout << std::endl;
+
+    ClapTrap limitTrap("LimitTrap");
+    limitTrap.takeDamage(15);
+    limitTrap.attack("Target5");
+    limitTrap.beRepaired(5);
+    std::cout << std::endl;
+    return 0;
 }

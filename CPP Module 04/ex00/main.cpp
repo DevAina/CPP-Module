733daa6a87e0cd82746501659d163d8c27ec6527
DevAina <ainatolotriniavo7@gmail.com>
2025-02-09 17:42:56 +0300

    Add: CPP Module 04/ex00: tester

--- CPP Module 04/ex00/main.cpp
+++ CPP Module 04/ex00/main.cpp
@@ -6,26 +6,70 @@
 /*   By: trarijam <trarijam@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 17:04:14 by trarijam          #+#    #+#             */
-/*   Updated: 2025/02/09 17:05:10 by trarijam         ###   ########.fr       */
+/*   Updated: 2025/02/09 17:41:41 by trarijam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
 
-int main()
+int main(void)
 {
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-	delete	meta;
-	delete j;
-	delete i;
-	return 0;
+    {
+        std::cout << "Test 1: Generic Animal" << std::endl;
+        Animal* animal = new Animal();
+        animal->makeSound();
+        std::cout << "Type: " << animal->getType() << std::endl;
+        delete animal;
+        std::cout << std::endl;
+    }
+
+    {
+        std::cout << "Test 2: Dog" << std::endl;
+        Dog* dog = new Dog();
+        dog->makeSound();
+        std::cout << "Type: " << dog->getType() << std::endl;
+        delete dog;
+        std::cout << std::endl;
+    }
+
+    {
+        std::cout << "Test 3: Cat" << std::endl;
+        Cat* cat = new Cat();
+        cat->makeSound();
+        std::cout << "Type: " << cat->getType() << std::endl;
+        delete cat;
+        std::cout << std::endl;
+    }
+
+    {
+        std::cout << "Test 4: Polymorphism with Animal array" << std::endl;
+        Animal* animals[3];
+        animals[0] = new Dog();
+        animals[1] = new Cat();
+        animals[2] = new Animal();
+
+        for (int i = 0; i < 3; ++i) {
+            animals[i]->makeSound();
+            std::cout << "Type: " << animals[i]->getType() << std::endl;
+            delete animals[i];
+        }
+        std::cout << std::endl;
+    }
+
+    {
+        std::cout << "Test 5: Object copying" << std::endl;
+        Dog dog1;
+        Dog dog2 = dog1;
+        dog2.makeSound();
+        std::cout << "Type: " << dog2.getType() << std::endl;
+
+        Cat cat1;
+        Cat cat2 = cat1;
+        cat2.makeSound();
+        std::cout << "Type: " << cat2.getType() << std::endl;
+        std::cout << std::endl;
+    }
+
+    return (0);
 }

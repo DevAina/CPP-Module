2ea2003e24e4532924fc2e77fdaf2ff1198706e7
DevAina <ainatolotriniavo7@gmail.com>
2025-02-09 15:12:43 +0300

    Change: Module_01/ex04/replace.cpp algo

--- CPP Module 01/ex04/replace.cpp
+++ CPP Module 01/ex04/replace.cpp
@@ -6,7 +6,7 @@
 /*   By: trarijam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 06:42:57 by trarijam          #+#    #+#             */
-/*   Updated: 2024/11/15 07:02:04 by trarijam         ###   ########.fr       */
+/*   Updated: 2025/02/09 10:42:19 by trarijam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,22 @@ std::string	replace(std::string line, std::string old_word, std::string new_word
 {
 	std::string result;
 	size_t		pos = 0;
+	size_t		lenOldWord;
 
+	lenOldWord = old_word.length();
 	while (pos < line.length())
 	{
 		size_t	index_found = line.find(old_word, pos);
 
 		if (index_found != std::string::npos)
 		{
-			result.append(line, pos, index_found - pos);
-			result.append(new_word);
-			pos = index_found + old_word.length();
+			result += line.substr(pos, index_found - pos);
+			result += new_word;
+			pos = index_found + lenOldWord;
 		}
 		else
 		{
-			result.append(line.substr(pos));
+			result += line.substr(pos);
 			break;
 		}
 	}
